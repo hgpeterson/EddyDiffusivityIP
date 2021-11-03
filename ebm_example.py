dde94ffe3ac8ff09b5233af62c362a15947a0526
Henry Peterson <hgpeterson@caltech.edu>
2021-11-03 14:29:20 -0700

    example with real data

--- ebm_example.py
+++ ebm_example.py
@@ -4,42 +4,34 @@ from ebm import EBM
 
 plt.style.use("plots.mplstyle")
 
-# setup test problem
-n = 2**8
-x = np.linspace(-1, 1, n)
-Q = 1365/np.pi*np.cos(np.arcsin(x))
-Q -= np.trapz(Q, x)/2
-D = 2.6e-4*np.ones(n)
-hs = hn = 2.4e5
+# # test problem
+# n = 2**8
+# x = np.linspace(-1, 1, n)
+# Q = 1365/np.pi*np.cos(np.arcsin(x))
+# Q -= np.trapz(Q, x)/2
+# D = 2.6e-4*np.ones(n)
+# hs = hn = 2.4e5
+
+# real data
+data = np.load("data/h_Q_CNRM-CM5.npz")
+h = data["h"]
+x = data["x"]
+Q = data["Q"]
+hs = h[0]
+hn = h[-1]
+D = 2.6e-4*np.ones(len(x))
 
 # solve
 ebm = EBM(x, Q, D, hs, hn)
-h = ebm.solve()
+h_tilde = ebm.solve()
 
 # plot
-plt.plot(x, h/1e3)
+plt.plot(x, h_tilde/1e3, label="model")
+plt.plot(x, h/1e3, label="truth")
 plt.xlabel("$x$")
 plt.ylabel("$h$ (kJ kg$^{-1}$)")
+plt.legend()
 plt.tight_layout()
 plt.savefig("h.png")
 print("h.png")
 plt.close()
-
-# # real data
-# data = np.load("Ts.npz")
-# Ts = data["Ts"]
-# lat = data["lat"]
-# n = len(lat)
-
-# h = h(Ts)
-
-# x = np.sin(lat*np.pi/180)
-# # Q = 1365/np.pi*np.cos(np.arcsin(x))
-# # Q -= np.trapz(Q, x)/2
-# # D = 0.649*np.ones(n) # W m-2 K-1
-# # hs = Ts[0]
-# # hn = Ts[-1]
-# # ebm = EBM(x, Q, D)
-# # h = ebm.solve()
-# plt.plot(x, h/1e3)
-# plt.show()
