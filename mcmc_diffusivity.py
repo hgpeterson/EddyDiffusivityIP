e64e07fde5336631997f9e1d5e58de93514f11af
rnpatel4 <ronak.patel27@outlook.com>
2021-11-06 21:40:59 -0700

    chore: small updates

--- mcmc_diffusivity.py
+++ mcmc_diffusivity.py
@@ -8,7 +8,7 @@ Created on Sun Oct 31 17:13:36 2021
 """
 import numpy as np
 import random
-from matplotlib.pyplot import plt
+import matplotlib.pyplot as plt
 
 def data():
     """ Loads observed (or climate model) Moist Static Energy h. 
@@ -43,13 +43,13 @@ def log_likelihoods(u, u_star):
     # TO DO: Figure out how to define gamma so that we can standardize/weight
     # the observations when taking the weighted Euclidean norm to compute mismatch
     # between observations and model predictions 
-    gamma = np.diag(np.ones(180,)*0.1)  #Example for now
+    gamma = np.diag(np.ones(180,)*1.5)  #Example for now
         
     # Note: The following formulation does not include a penalty for u being 
     # far away from the prior... unless we get a well-enough defined prior where
     # this is reasonable 
-    move_llikelihood = 0.5 * np.sum(np.linalg.inv(gamma**2) * (y - model(u_star))**2)
-    previous_llikelihood =  0.5 * np.sum(np.linalg.inv(gamma**2) * (y - model(u))**2)
+    move_llikelihood = 0.5 * np.sum(np.linalg.inv(gamma**(1/2)) * (y - model(u_star))**2)
+    previous_llikelihood =  0.5 * np.sum(np.linalg.inv(gamma**(1/2)) * (y - model(u))**2)
     
     return move_llikelihood, previous_llikelihood
 
@@ -66,19 +66,19 @@ def main():
     
     
     # Sample from the prior distribution
-    mean = np.ones(180,)*1.5
+    mean = np.ones(180,)*9.5
     u_arr = mean
 
-    cov = np.diag(np.ones(180,)*0.1)  # diagonal covariance 
+#    cov = np.diag(np.ones(180,)*0.1)  # diagonal covariance 
     #(the choice of cov seems EXTREMELY important, or else MCMC won't converge...)
     # If we expect nondiagonal covariance, need to specify it
    
     # Example, if we know all points will have to be the same, bc of the way 
-    # this toy example is, made the cov matrix all 1s. It converges to the correct value
-    cov = np.ones((180,180))
+    # this toy example is, made the cov matrix all 0.1s. It converges to the correct value
+    cov = np.ones((180,180))*0.1
     u = np.random.multivariate_normal(mean, cov)
     
-    for i in range(2000):
+    for i in range(10000):
         # TO DO: Figure out how to sample from a Markov kernel to get u_star
         # Would this just consider a gaussian with mean of the previous u and then some covariance? 
         u_star = np.random.multivariate_normal(u, cov)
@@ -95,7 +95,11 @@ def main():
 
         u_arr = np.c_[u_arr, u] #Since u is a 1D vector, if we keep this appending method, make sure to append using the correct axis
     
-    plt.imshow(u_arr)
+    plt.plot(u_arr[1,:])
+    # See how the data looks after the burn-in period (burn-in period not a constant)
+    plt.plot(u_arr[1,250:])
+    plt.hist(u_arr[1,250:])
+    plt.xlim(0,10)
     
 if __name__ == '__main__':
     main()
