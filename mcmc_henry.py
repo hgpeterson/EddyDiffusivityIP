1b45e2d4d4d71ca1a0adc0bff52655e8bf7b2824
Henry Peterson <hgpeterson@sampo.gps.caltech.edu>
2021-11-22 17:44:20 -0800

    working on smooth prior

--- mcmc_henry.py
+++ mcmc_henry.py
@@ -3,6 +3,8 @@ import random
 import matplotlib.pyplot as plt
 from ebm import EBM
 from scipy.linalg import fractional_matrix_power
+from scipy.special import legendre
+import os
 
 random.seed(42)
 
@@ -26,7 +28,7 @@ def data():
 
     return h, x, Q, hs, hn
 
-def model(x, Q, D, hs, hn, spectral=True):
+def model(x, Q, D, hs, hn, spectral):
     """ Define the forward model, or just import it as a separate python 
             function with documented inputs/outputs
     Arguments:
@@ -35,11 +37,22 @@ def model(x, Q, D, hs, hn, spectral=True):
     # Start with simple model
     ebm = EBM(x, Q, D, hs, hn, spectral)
     h_tilde = ebm.solve()
-    D = ebm.D
     
-    return np.squeeze(h_tilde), D
+    return np.squeeze(h_tilde), ebm.D
 
-def log_likelihoods(gamma_inv_12, u, u_star, h, x, Q, hs, hn):
+def matrix_norm(matrix, vector):
+    """ Computes the matrix norm <vector, vector>_matrix in the L2 norm.
+
+    Args:
+        matrix (n x n array) 
+        vector (n x 1 array)
+
+    Returns:
+        (float)
+    """
+    return np.linalg.norm(np.dot(matrix, vector))
+
+def log_likelihoods(gamma_inv_12, C_inv_12, m, u, u_star, h, x, Q, hs, hn, spectral):
     """ Computes the ratio of posterior probabilities from u_star/u  
             using observations and forward model evaluation. This provides part
             of the acceptance probability
@@ -47,65 +60,71 @@ def log_likelihoods(gamma_inv_12, u, u_star, h, x, Q, hs, hn):
             move_llikelihood (float): negative log likelihood of the proposed move to u_star
             previous_llikelihood  (float): negative log likelihood of the existing u  
     """
-        
-    # Note: The following formulation does not include a penalty for u being 
-    # far away from the prior... unless we get a well-enough defined prior where
-    # this is reasonable 
-    move_llikelihood     = -0.5 * np.sum(np.dot(gamma_inv_12, (h - model(x, Q, u_star, hs, hn)[0])))**2
-    previous_llikelihood = -0.5 * np.sum(np.dot(gamma_inv_12, (h - model(x, Q, u, hs, hn)[0])))**2
+    
+    move = model(x, Q, u_star, hs, hn, spectral)
+    prev = model(x, Q, u, hs, hn, spectral)
+    move_llikelihood     = -0.5*matrix_norm(gamma_inv_12, h - move[0])**2 \
+                           -0.5*matrix_norm(C_inv_12, m - u_star)**2
+    previous_llikelihood = -0.5*matrix_norm(gamma_inv_12, h - prev[0])**2 \
+                           -0.5*matrix_norm(C_inv_12, m - u)**2
+
+    # non-negative D
+    if np.any(move[1] < 0):
+        move_llikelihood = -np.inf
     
     return move_llikelihood, previous_llikelihood
 
-
 def main():
-    # TO DO:
-    # 1) Initalize markov kernel from a multivariate Gaussian with constant mean and cleverly chosen covariance   
-    # 2) Propose a new parameter u*_(n+1)
-    # 3) Set u_(n+1) = u∗_(n+1) with probability a(un, u∗_(n+1)); otherwise set u_(n+1) = u_n
-    #   Consider definining the acceptance probability like Eq 2.15 in https://clima.caltech.edu/files/2020/01/Calibrate-Emulate-Sample-2021.pdf 
-    #   This is also just the log likelihood, evaluating with a forward model
-    # 4) After the burn-in period, consider the distribution of 1000+ samples to estimate u and do UQ
-    # 5) Visualize the distribution of u (diffusivity) lat on abscissa, D on ordinate (or flipped, idc), and heatmap-like shading based on density of samples
-    
     # Load data
     h, x, Q, hs, hn = data()
 
     # pre-compute Gamma
-    gamma = np.identity(len(x))
+    gamma = 1e0*np.identity(len(x))
     gamma_inv_12 = fractional_matrix_power(gamma, -1/2)
 
     # is gamma positive definite?
     # print(np.all(np.linalg.eigvals(gamma) > 0))
 
-    # Sample from the prior distribution
+    # spectral prior
+    spectral = True
     n_polys = 5
-    mean = np.zeros(n_polys)
-    mean[0] = 2.6e-4
-    mean[4] = -1e-4
-    u_arr = mean
+    m = np.zeros(n_polys)
+    m[0] = 2.6e-4
+    m[4] = -1e-4
+    C = 1e-11*np.identity(len(m))
+    C_inv_12 = fractional_matrix_power(C, -1/2)
 
-    # Try a sample n-diagonal covariance matrix 
-    cov = 1e-10*np.identity(len(mean))
-    u = np.random.multivariate_normal(mean, cov)
+    # # pointwise prior
+    # spectral = False
+    # # m = 2.6e-4*legendre(0)(x) - 1e-4*legendre(4)(x)
+    # m = 2.7e-4*legendre(0)(x) - 0.9e-4*legendre(4)(x)
+    # C = 1e-13*np.identity(len(m))
+    # C_inv_12 = fractional_matrix_power(C, -1/2)
+
+    # iteration number 
+    N = 200
     
-    N = 1000
-    h_tildes = np.zeros((N, len(x)))
-    Ds = np.zeros((N, len(x)))
+    # start solution arrays
+    us = np.zeros((N + 1, len(m)))
+    h_tildes = np.zeros((N + 1, len(x)))
+    Ds = np.zeros((N + 1, len(x)))
+
+    # initialize at mean
+    u = m
+    
+    # main loop
     for i in range(N):
-        # TO DO: Figure out how to sample from a Markov kernel to get u_star
-        # Would this just consider a gaussian with mean of the previous u and then some covariance? 
-        
-        h_tildes[i, :], Ds[i, :] = model(x, Q, u, hs, hn)
+        h_tildes[i, :], Ds[i, :] = model(x, Q, u, hs, hn, spectral)
+        us[i, :] = u
 
         if i % 100 == 0:
             print(f"{i}/{N}")
 
-        u_star = np.random.multivariate_normal(u, cov)
+        u_star = np.random.multivariate_normal(u, C)
         
-        move, previous = log_likelihoods(gamma_inv_12, u, u_star, h, x, Q, hs, hn)
-        a = np.min([previous/move, 1])
-
-        print(f"{previous:1.1e}")
+        move, prev = log_likelihoods(gamma_inv_12, C_inv_12, m, u, u_star, h, x, Q, hs, hn, spectral)
+        print(f"{move:1.1e}, {prev:1.1e}")
+        a = np.min([prev/move, 1])
         
         if a == 1:
             u = u_star
@@ -114,19 +133,41 @@ def main():
         else:
             u = u
 
-        u_arr = np.c_[u_arr, u] #Since u is a 1D vector, if we keep this appending method, make sure to append using the correct axis
+    # final solution
+    h_tildes[N, :], Ds[N, :] = model(x, Q, u, hs, hn, spectral)
+    us[N, :] = u
+    if spectral:
+        print(u)
 
-    # print final solution
-    print(u)
+    # save data
+    np.savez("out.npz", N=N, x=x, h=h, us=us, h_tildes=h_tildes, Ds=Ds, m=m, C_inv_12=C_inv_12, gamma_inv_12=gamma_inv_12)
+    print("out.npz")
+
+def plots():
+    # load data
+    d= np.load("out.npz")
+    N = d["N"]
+    x = d["x"]
+    h = d["h"]
+    us = d["us"]
+    h_tildes = d["h_tildes"]
+    Ds = d["Ds"]
+    m = d["m"]
+    C_inv_12 = d["C_inv_12"]
+    gamma_inv_12 = d["gamma_inv_12"]
     
     # Plot sum of squared errors in h over time
     fig, ax = plt.subplots()
-    errors = np.zeros(N)
-    for i in range(1, N):
-        errors[i] = np.sum(np.dot(gamma_inv_12, (h - h_tildes[i, :])))**2
-    ax.semilogy(np.abs(errors))
+    errors_h = np.zeros(N)
+    errors_u = np.zeros(N)
+    for i in range(1, N+1):
+        errors_h[i-1] = -0.5*matrix_norm(gamma_inv_12, h - h_tildes[i, :])**2
+        errors_u[i-1] = -0.5*matrix_norm(C_inv_12, m - us[i, :])**2
+    ax.semilogy(np.abs(errors_h), label=r"$\frac{1}{2} || h - \tilde h ||_\Gamma^2$")
+    ax.semilogy(np.abs(errors_u), label=r"$\frac{1}{2} || u - m ||_C^2$")
+    ax.legend()
     ax.set_xlabel("iteration")
-    ax.set_ylabel(r"model error $|| y - G(u) ||_\Gamma^2$")
+    ax.set_ylabel(r"error")
     # plt.savefig("error.pdf")
     # print("error.pdf")
     plt.savefig("error.png")
@@ -154,6 +195,7 @@ def main():
     D_true = d["D"]
     ax.plot(x, 1e4*D_true, label="reference model")
     ax.plot(x, 1e4*Ds[-1, :], label="inverse result")
+    ax.plot(x, 1e4*Ds[0, :], "--", label="init. cond.")
     ax.legend()
     ax.set_xlabel(r"latitude $\phi$ (degrees)")
     ax.set_xticks(np.sin(np.deg2rad(np.arange(-90, 91, 10))))
@@ -167,3 +209,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    plots()
